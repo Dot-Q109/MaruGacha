382a93328ccc995e6d1c0c64d08b0a26b921bdc0
Dot-Q <Dot.Q109@gmail.com>
2023-03-18 21:19:17 +0900

    feat:動くところまで仮実装

---
+++ src/about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>このサイトについて - 丸亀ガチャ</title>
+</head>
+
+<body>
+    <header>
+        <a href="/">丸亀ガチャ</a>
+    </header>
+    <div>
+        ◆このサイトについて<br>
+        ◆メニューについて<br>
+        ◆免責事項<br>
+        ◆Cookieについて<br>
+    </div>
+    <footer>
+        <a href="about.php">このサイトについて</a>
+    </footer>
+</body>
+
+</html>
