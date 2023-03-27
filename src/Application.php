aadc4794880db62dc66f17206af36a7f0fefe1d5
Dot-Q <Dot.Q109@gmail.com>
2023-03-27 23:06:29 +0900

    docs:不要なコメントの削除

--- src/Application.php
+++ src/Application.php
@@ -36,14 +36,6 @@ class Application
             ]
         );
 
-        // $this->databaseManager->connectToMySQL(
-        //     [
-        //         'hostname' => $_ENV['DB_HOST'],
-        //         'username' => $_ENV['DB_USERNAME'],
-        //         'password' => $_ENV['DB_PASSWORD'],
-        //         'database' => $_ENV['DB_DATABASE'],
-        //     ]
-        // );
     }
 
 
